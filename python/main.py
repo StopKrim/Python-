ac4a2af6b1cd8b57010a5619228e386e196fecd2
KonkerE <sasakikojiro@inbox.ru>
2022-07-16 16:41:55 +0300

    add file

--- python/main.py
+++ python/main.py
@@ -1,3 +1,5 @@
+# 16.07.2022
+
 name = "Eric"
 message = f"Hello {name}, would you like to learn some Python today?"
 print(message)
@@ -27,3 +29,17 @@ print(name.strip())
 name = '  albert    '
 print(name.rstrip())
 
+# 2.8
+print(5 + 3)
+
+print(13 - 5)
+
+print(2 * 4)
+
+print(16 / 2)
+
+number = 7
+print(number)
+
+# Дзен Python
+import this

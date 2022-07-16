61aac691855af1c10a714027969559878894fb2b
KonkerE <sasakikojiro@inbox.ru>
2022-07-16 12:43:55 +0300

    add file

--- python/main.py
+++ python/main.py
@@ -1,3 +1,29 @@
 name = "Eric"
 message = f"Hello {name}, would you like to learn some Python today?"
 print(message)
+
+name = "Fil"
+print(name.lower())
+
+name = "fil"
+print(name.title())
+
+name = "Fil"
+print(name.upper())
+
+
+print('Albert Einstein once said, "A person who never made \na mistare never tried anything new.')
+
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, "A person who never made \na mistare never tried anything new.'
+print(message)
+
+name = '  albert    '
+print(name.lstrip())
+
+name = '  albert    '
+print(name.strip())
+
+name = '  albert    '
+print(name.rstrip())
+

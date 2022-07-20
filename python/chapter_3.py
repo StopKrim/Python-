c3e85d8cf9a104db229761c87f5737d0ef422daa
KonkerE <sasakikojiro@inbox.ru>
2022-07-20 20:52:20 +0300

    add file

---
+++ python/chapter_3.py
@@ -0,0 +1,28 @@
+# 3.1
+names = ['Eric', 'David', 'Elizabet', 'Met', 'Fill']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[-1])
+
+# 3.2
+message = f'My very first and best friend is {names[0].title()}!'
+print(message)
+
+message = f'My very first and best friend is {names[1].title()}!'
+print(message)
+
+message = f'My very first and best friend is {names[2].title()}!'
+print(message)
+
+message = f'My very first and best friend is {names[3].title()}!'
+print(message)
+
+message = f'My very first and best friend is {names[4].title()}!'
+print(message)
+
+# 3.3
+cars = ['toyota', 'bmw', 'audi', 'lexus', 'honda', 'nissan']
+message = f'My favorite car brand is {cars[3].title()}.'
+print(message)
